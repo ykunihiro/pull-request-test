96dbc5533312d4c6ab14e36d9835976dbd2b994a
ykunihiro <Yuto@YutoMac.local>
2016-12-14 09:29:28 +0900

    配列をソートする処理を追加しました

--- sort.js
+++ sort.js
@@ -1,9 +1,9 @@
 var sortNumber = function (number) {
    number.sort(function (a, b) {
-	if(a==b) {
-	  return 0;
-	}
-	return a < b ? -1 : 1;
+        if(a==b) {
+          return 0;
+        }
+        return a < b ? -1 : 1;
    });
 };
 
@@ -12,4 +12,3 @@ var sortNumber = function (number) {
 var number = {19, 3, 81, 1, 24, 21};
 sortNumber(number);
 console.log(number);
-
